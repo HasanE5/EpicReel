1e6522eca9b93549149f1056f5c08a09afe6a763
HasanE <hasaneskaik@gmail.com>
2026-02-01 10:16:24 +0200

    add posters and movies From HasanE..

--- app/src/main/java/com/hasan/ahmed/belal/epic_reel/MainActivity.kt
+++ app/src/main/java/com/hasan/ahmed/belal/epic_reel/MainActivity.kt
@@ -41,22 +41,22 @@ class MainActivity : AppCompatActivity() {
         val movies = mutableListOf<Movies>()
         movies.add(
             Movies(
-                "Interstellar",
-                R.drawable.interstellar,
-                "2014",
-                "2h 18m",
-                "4.9",
-                "Interstellar is about astronauts searching for a new home for humanity while a father tries to reunite with his daughter."
+                "avengers",
+                R.drawable.avengers_endgame_poster,
+                "2019",
+                "3h 1m",
+                "4.6",
+                "Avengers: Endgame* is about the heroes fighting Thanos. They bring back their friends, and Iron Man saves everyone."
             )
         )
         movies.add(
             Movies(
-                "The Dark knight",
+                "The Dark Knight",
                 R.drawable.the_dark_knight,
                 "2008",
-                "2h 18m",
-                "4.8",
-                "The Dark knight is about astronauts searching for a new home for humanity while a father tries to reunite with his daughter"
+                "2h 32m",
+                "4.7",
+                "The Dark Knight is about Batman fighting the Joker, who wants to bring chaos to Gotham City."
             )
         )
         val adapter = MoviesAdapter(movies)
@@ -68,8 +68,60 @@ class MainActivity : AppCompatActivity() {
 
         trendingMovies.add(
             Movies(
+                "Avengers\nEndgame",
+                R.drawable.avengers_endgame_poster,
+                "2019",
+                "3h 1m",
+                "4.6",
+                "Avengers: Endgame* is about the heroes fighting Thanos. They bring back their friends, and Iron Man saves everyone."
+            )
+        )
+
+        trendingMovies.add(
+            Movies(
+                "Star Wars VI",
+                R.drawable.star_wars_episode_vi_poster,
+                "1983",
+                "2h 12m",
+                "4.2",
+                "Return of the Jedi is about the heroes stopping the evil Empire. Luke faces Darth Vader and the Emperor, while the Rebels destroy the new Death Star. In the end, Vader saves Luke and the galaxy is free."
+            )
+        )
+        trendingMovies.add(
+            Movies(
+                "Star Wars III",
+                R.drawable.star_wars_episode_iii_poster,
+                "2005",
+                "2h 20m",
+                "3.3",
+                "Star Wars: Episode III – Revenge of the Sith is about Anakin becoming Darth Vader and the Jedi being destroyed."
+            )
+        )
+        trendingMovies.add(
+            Movies(
+                "Avengers\nAge Of Ultron",
+                R.drawable.avengers_age_of_ultron_poster,
+                "2015",
+                "2h 21m",
+                "3.7",
+                "Avengers: Age of Ultron is about the heroes fighting a powerful robot named Ultron. Ultron wants to destroy humanity, so the Avengers join forces to stop him and save the world."
+            )
+        )
+        trendingMovies.add(
+            Movies(
+                "Avengers\nInfinity War",
+                R.drawable.avengers_infinity_war_poster,
+                "2018",
+                "2h 29m",
+                "4.2",
+                "Avengers: Infinity War is about the heroes trying to stop Thanos. He collects the Infinity Stones to wipe out half of all life. In the end, Thanos wins and many heroes disappear."
+            )
+        )
+
+        trendingMovies.add(
+            Movies(
                 "Interstellar",
-                R.drawable.interstellar_poster,
+                R.drawable.interstellar,
                 "2014",
                 "2h 18m",
                 "4.9",
@@ -79,47 +131,7 @@ class MainActivity : AppCompatActivity() {
         trendingMovies.add(
             Movies(
                 "The Dark knight",
-                R.drawable.the_dark_knight_poster,
-                "2008",
-                "2h 18m",
-                "4.8",
-                "The Dark knight is about astronauts searching for a new home for humanity while a father tries to reunite with his daughter"
-            )
-        )
-        trendingMovies.add(
-            Movies(
-                "Interstellar",
-                R.drawable.interstellar_poster,
-                "2014",
-                "2h 18m",
-                "4.9",
-                "Interstellar is about astronauts searching for a new home for humanity while a father tries to reunite with his daughter."
-            )
-        )
-        trendingMovies.add(
-            Movies(
-                "The Dark knight",
-                R.drawable.the_dark_knight_poster,
-                "2008",
-                "2h 18m",
-                "4.8",
-                "The Dark knight is about astronauts searching for a new home for humanity while a father tries to reunite with his daughter"
-            )
-        )
-        trendingMovies.add(
-            Movies(
-                "Interstellar",
-                R.drawable.interstellar_poster,
-                "2014",
-                "2h 18m",
-                "4.9",
-                "Interstellar is about astronauts searching for a new home for humanity while a father tries to reunite with his daughter."
-            )
-        )
-        trendingMovies.add(
-            Movies(
-                "The Dark knight",
-                R.drawable.the_dark_knight_poster,
+                R.drawable.the_dark_knight,
                 "2008",
                 "2h 18m",
                 "4.8",
@@ -136,67 +148,46 @@ class MainActivity : AppCompatActivity() {
         val releasesMovies = mutableListOf<Movies>()
         releasesMovies.add(
             Movies(
-                "The Dark knight",
-                R.drawable.the_dark_knight_poster,
-                "2008",
-                "2h 18m",
-                "4.8",
-                "The Dark knight is about astronauts searching for a new home for humanity while a father tries to reunite with his daughter"
+                "Scream 7",
+                R.drawable.scream7_poster,
+                "2026",
+                "1h 54m",
+                "NEW",
+                "Scream 7: is about a new Ghostface killer who goes after Sidney Prescott’s daughter."
             )
         )
 
         releasesMovies.add(
             Movies(
-                "Interstellar",
-                R.drawable.interstellar_poster,
-                "2014",
-                "2h 18m",
-                "4.9",
-                "Interstellar is about astronauts searching for a new home for humanity while a father tries to reunite with his daughter."
+                "Super Mario\nGalaxy",
+                R.drawable.super_mario_poster,
+                "2026",
+                "unknown",
+                "NEW",
+                "Super Mario Galaxy",
+                )
+        )
+        releasesMovies.add(
+            Movies(
+                "Zootopia 2",
+                R.drawable.zootopia2_poster,
+                "2025",
+                "1h 48m",
+                "3.4",
+                "Zootopia 2 is about Judy and Nick stopping a snake villain in the city.",
             )
         )
         releasesMovies.add(
             Movies(
-                "The Dark knight",
-                R.drawable.the_dark_knight_poster,
-                "2008",
-                "2h 18m",
-                "4.8",
-                "The Dark knight is about astronauts searching for a new home for humanity while a father tries to reunite with his daughter"
+                "Odyssey",
+                R.drawable.odyssey_poster,
+                "2026",
+                "unknown",
+                "0.0",
+                "The Odyssey (2026) is about Odysseus trying to get home after war, facing many dangers.",
             )
         )
 
-        releasesMovies.add(
-            Movies(
-                "Interstellar",
-                R.drawable.interstellar_poster,
-                "2014",
-                "2h 18m",
-                "4.9",
-                "Interstellar is about astronauts searching for a new home for humanity while a father tries to reunite with his daughter."
-            )
-        )
-        releasesMovies.add(
-            Movies(
-                "The Dark knight",
-                R.drawable.the_dark_knight_poster,
-                "2008",
-                "2h 18m",
-                "4.8",
-                "The Dark knight is about astronauts searching for a new home for humanity while a father tries to reunite with his daughter"
-            )
-        )
-
-        releasesMovies.add(
-            Movies(
-                "Interstellar",
-                R.drawable.interstellar_poster,
-                "2014",
-                "2h 18m",
-                "4.9",
-                "Interstellar is about astronauts searching for a new home for humanity while a father tries to reunite with his daughter."
-            )
-        )
 
         val releasesMoviesAdapter = ReleasesMoviesAdapter(releasesMovies)
         binding.releasesMovies.adapter = releasesMoviesAdapter
